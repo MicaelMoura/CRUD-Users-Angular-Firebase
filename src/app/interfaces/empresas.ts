b54f8685461297d3451992545f0fd48677b3f7cc
Jonatas Silva <jads.adelino@gmail.com>
2025-09-23 20:42:52 -0300

    criação de modulo empresas

---
+++ src/app/interfaces/empresas.ts
@@ -0,0 +1,12 @@
+export interface Empresas {
+    razaoSicial: string,
+    nomeFantasia: string,
+    cnpj: string,
+    endereco: string,
+    telefone: string,
+    email: string
+    Cidade?: string,
+    Bairro?: string,
+    Cep?: string,
+    Complemento?: string
+}
---
+++ src/app/pages/empresas/empresas.component.ts
@@ -0,0 +1,62 @@
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpresasService } from '../../services/empresas.service'; 
+import { Empresas } from '../../interfaces/empresas'; 
+import e from 'express';
+import { ModalEmpresasFormComponent } from './modal-form-empresas/modal-form-empresas.component';
+
+@Component({
+  selector: 'app-empresas',
+  templateUrl: './empresas.component.html',
+  styleUrls: ['./empresas.component.scss']
+})
+export class EmpresasComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'cnpj', 'action'];
+  dataSource!: MatTableDataSource<Empresas>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  private empresasService: EmpresasService = inject(EmpresasService);
+  constructor(public dialog: MatDialog) {
+    this.dataSource = new MatTableDataSource<Empresas>([]);
+  }
+
+  ngOnInit(): void {
+    this.getListEmpresas();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openModalAddCompany() {
+    this.dialog.open(ModalEmpresasFormComponent, {
+      width: '900px'
+    })
+    .afterClosed().subscribe(() => {
+      this.getListEmpresas();
+    })
+  }
+
+  openModalViewCompany(company: Empresas) {
+    // Lógica para abrir o modal de visualização de empresa
+  }
+
+  deleteCompany(companyId: string) {
+    // Chama o método de exclusão do serviço
+    this.empresasService.deleteEmpresa(companyId);
+  }
+
+  getListEmpresas() {
+     this.empresasService.getEmpresas().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+}
